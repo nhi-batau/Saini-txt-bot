9a5bde66cb0ec223bcfd25cc836b6050268a2fe4
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2025-03-07 22:37:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ def hello_world():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/01/29/Purvi-Cid-1.png">
+	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
     <titleSudoR2spr Repository</title>
-	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/01/29/Purvi-Cid-1.png">
+	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
 
 </head>
 
 <body>
     <div class="container" style="bg-dark text-red text-center py-3 mt-5">
-        <a href="https://t.me/+taZCy5OkoMUwNzI9" class="card">
+        <a href="https://github.com/nikhilsaini098" class="card">
             <p>
-               >▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
+              />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
 	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
               />██▄▄▄▀▀█░▀▀░██░███░█░█░██░███<br 
 	      />██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br 
@@ -33,9 +33,9 @@ def hello_world():
     </div>
 	<br></br><br></br><br></br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
-	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/01/29/Purvi-Cid-1.png" width="60" height="60">
+	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
         Powered By SAINI 
-		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/01/29/Purvi-Cid-1.png" width="60" height="60">
+		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
 		<div class="footer__copyright">
             <p class="footer__copyright-info">
                 © 2024 Video Downloader. All rights reserved.
